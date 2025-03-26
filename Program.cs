7bed6575d74fd73a4f7d6b3fe36cd2c737d63415
victor henrique <victorcardosoferreirahenrique@gmail.com>
2025-03-26 18:37:28 -0400

    primeiro exercicio sobre poo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Carro carro1 = new Carro();
+        carro1.marca= "BMW";
+        carro1.modelo= "n sei um modelo";
+        carro1.ano=2024;
+
+        carro1.ExibirInformacoes();
+    }
+}
---
+++ carro.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Carro {
+    public string marca;
+    public string modelo;
+    public int ano;
+
+    public void ExibirInformacoes(){
+        Console.WriteLine($"Carro: {marca} Modelo: {modelo} Ano: {ano}");
+    }
+}
